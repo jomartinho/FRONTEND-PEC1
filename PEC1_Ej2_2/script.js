aad87ddd69c9c9a2b2829043b3c78f798c571d85
joao rebordao martinho <jmartinho@uoc.edu>
2024-10-08 16:43:56 +0100

    Commiting final script

--- PEC1_Ej2_2/script.js
+++ PEC1_Ej2_2/script.js
@@ -6,20 +6,50 @@ const amountEl_two = document.getElementById('amount-two');
 const rateEl = document.getElementById('rate');
 const swap = document.getElementById('swap');
 
+
+
+const loadingMessage = document.createElement('p')
+loadingMessage.id = "loading";
+loadingMessage.innerText = "Fetching rates... Please Wait.";
+loadingMessage.style.display = 'none';
+document.body.appendChild(loadingMessage);
+
+
+
+amountEl_one.addEventListener('input', () => {
+    if (amountEl_one.value < 0) {
+        amountEl_one.value = 0;
+        alert('Negative numbers are not allowed.');
+    }
+})
+
 function calculate() {
  const currency_one = currencyEl_one.value;
  const currency_two = currencyEl_two.value;
 
+ loadingMessage.style.display = 'block';
+ rateEl.innerText = '';
+
 fetch(`https://open.er-api.com/v6/latest/${currency_one}`)
-    .then(res => res.json())
+    .then(res => {
+        if (!res.ok) {
+            throw new Error('Error fetching exchange rates');
+         }
+         return res.json();
+        })
     .then(data => {
-        // console.log(data);
         const rate = data.rates[currency_two];
 
         rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
         
         amountEl_two.value = (amountEl_one.value * rate).toFixed(2);
-    });
+    })
+    .catch(error => {
+        rateEl.innerText = `Error: ${error.message}`;
+    })
+    .finally(() => {
+        loadingMessage.style.display = 'none';
+    })
 }
 
 currencyEl_one.addEventListener('change', calculate);
